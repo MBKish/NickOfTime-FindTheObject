5867c6540760d2ea46c356c176ce0fbb288bfe00
Matthew Kish <matthewbkish@gmail.com>
2013-08-13 16:32:33 -0500

    Initial Commit

---
+++ NickOfTime-FindTheObject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NickOfTime-FindTheObject
+//
+//  Created by Matthew Kish on 8/13/13.
+//  Copyright (c) 2013 TimeMonkeys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NickOfTime-FindTheObject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NickOfTime-FindTheObject
+//
+//  Created by Matthew Kish on 8/13/13.
+//  Copyright (c) 2013 TimeMonkeys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
